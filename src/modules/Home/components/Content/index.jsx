cf8c6ee635cc4c6f2fdb1cb291d1e1b56bd3f7f0
LastGenius-edu <as.andreysultanov@gmail.com>
2021-05-17 20:32:37 +0300

    Add dynamic content to the home page

--- src/modules/Home/components/Content/index.jsx
+++ src/modules/Home/components/Content/index.jsx
@@ -2,213 +2,247 @@ import React from "react";
 
 import Card from "../../../Store/components/Card";
 
-const Content = () => {
-  return (
-    <div>
-      <section id="ijk2r" class="flex-sect">
-        <div class="container-width">
-          <div id="irdvr" class="flex-title">
-            Our Best Beers
-            <br />
-          </div>
-          <div id="i0gcu" class="flex-desc">
-            Tasty and different, all at one touch of your fingertips away
-            <br />
-          </div>
-          <div class="cards">
-          <Card
-            imgurl="https://media.istockphoto.com/photos/glass-of-dark-beer-picture-id910481830?k=6&m=910481830&s=612x612&w=0&h=qy-FCnlabygw3g1J1oQqefofkLAlxIQwNtb7axPAdqk="
-            name="Half-Baked Supine Spinal Twist Flanders Red Ale"
-            description="Fragrant giggle juice like an old cup of office coffee/div"
-          />
-          <Card
-            imgurl="https://image.freepik.com/free-photo/beer-mug-wooden-table_8119-993.jpg"
-            name="Asian-Spiced Eggbeater Pedal Brown Ale"
-            description="Long-tailed bouquet with an 'it's complicated' relationship status"
-          />
-          <Card
-            imgurl="https://beerbusinessfinance.com/wp-content/uploads/2020/07/Depositphotos_205388102_s-2019.jpg"
-            name="Aïoli Camel Toe Maibock"
-            description="Velvety hop bouquet like shit and blueberries got together and
-                had a baby"
-          />
-          <Card
-            imgurl="https://dogfishalehouse.com/wp-content/uploads/2015/04/brownaleblog.jpg"
-            name="White Pepper Dusted Thunderbolt Dortmunder"
-            description="Cloying flavor with homicidal tendencies"
-          />
-          <Card
-            imgurl="https://2fdltvvn8wp2rn64ywgk8o17-wpengine.netdna-ssl.com/wp-content/uploads/2015/07/american-brown-ale-style.jpg"
-            name="Grandma's Apple Pie Brake Lever Altbier"
-            description="Golden breakfast of champions reminiscent of a wet old box in
-            your grandma's basement"
-          />
-          <Card
-            imgurl="https://media.istockphoto.com/photos/mug-fresh-beer-with-cap-of-foam-on-black-background-picture-id160895488?k=6&m=160895488&s=612x612&w=0&h=gtPbN7fz3s1dh82SUIxLJVafaFbmBG6mn35RS3k79V8="
-            name="Deep-Fried Doppelgänger Brown Ale"
-            description="Dank disappointment that demands slow savoring"
-          />
-          </div>
-        </div>
-      </section>
-      <section id="iwj3k" class="blk-sect">
-        <div class="container-width">
-          <div id="ivwuf" class="blk-title">
-            beerok Subscriptions
-            <br />
-          </div>
-          <div id="i5byh" class="blk-desc">
-            Subscribe to our club and get beer every month
-            <br />
-          </div>
-          <div class="price-cards">
-            <div class="price-card-cont">
-              <div class="price-card">
-                <div id="i0fki" class="pc-title">
-                  Private Beer
-                  <br />
-                </div>
-                <div id="irr1l" class="pc-desc">
-                  If you want to get a taste
-                  <br />
-                </div>
-                <div id="iub2s" class="pc-feature odd-feat">
-                  + Hand-picked beer selections each month
-                  <br />
-                </div>
-                <div id="i0wmu" class="pc-feature">
-                  + 6 litres of the best beer per month
-                </div>
-                <div id="ii3u5h" class="pc-feature">
-                  + Stories of how the beer was made
-                  <br />
-                </div>
-                <div id="i38a8" class="pc-amount odd-feat">
-                  $ 29,90/mo
-                </div>
-              </div>
+class Content extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [
+        {
+          imgurl:
+            "https://media.istockphoto.com/photos/glass-of-dark-beer-picture-id910481830?k=6&m=910481830&s=612x612&w=0&h=qy-FCnlabygw3g1J1oQqefofkLAlxIQwNtb7axPAdqk=",
+          name: "Half-Baked Supine Spinal Twist Flanders Red Ale",
+          description: "Fragrant giggle juice like an old cup of office coffee",
+        },
+        {
+          imgurl:
+            "https://image.freepik.com/free-photo/beer-mug-wooden-table_8119-993.jpg",
+          name: "Asian-Spiced Eggbeater Pedal Brown Ale",
+          description:
+            "Long-tailed bouquet with an 'it's complicated' relationship status",
+        },
+        {
+          imgurl:
+            "https://beerbusinessfinance.com/wp-content/uploads/2020/07/Depositphotos_205388102_s-2019.jpg",
+          name: "Aïoli Camel Toe Maibock",
+          description:
+            "Velvety hop bouquet like shit and blueberries got together and had a baby",
+        },
+        {
+          imgurl:
+            "https://dogfishalehouse.com/wp-content/uploads/2015/04/brownaleblog.jpg",
+          name: "White Pepper Dusted Thunderbolt Dortmunder",
+          description: "Cloying flavor with homicidal tendencies",
+        },
+        {
+          imgurl:
+            "https://2fdltvvn8wp2rn64ywgk8o17-wpengine.netdna-ssl.com/wp-content/uploads/2015/07/american-brown-ale-style.jpg",
+          name: "Grandma's Apple Pie Brake Lever Altbier",
+          description:
+            "Golden breakfast of champions reminiscent of a wet old box in your grandma's basement",
+        },
+        {
+          imgurl:
+            "https://media.istockphoto.com/photos/mug-fresh-beer-with-cap-of-foam-on-black-background-picture-id160895488?k=6&m=160895488&s=612x612&w=0&h=gtPbN7fz3s1dh82SUIxLJVafaFbmBG6mn35RS3k79V8=",
+          name: "Deep-Fried Doppelgänger Brown Ale",
+          description: "Dank disappointment that demands slow savoring",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <section id="ijk2r" class="flex-sect">
+          <div class="container-width">
+            <div id="irdvr" class="flex-title">
+              Our Best Beers
+              <br />
             </div>
-            <div class="price-card-cont">
-              <div id="iv8vn" class="price-card pc-regular">
-                <div id="i57yh" class="pc-title">
-                  Lieutenant Beer
-                  <br />
-                </div>
-                <div id="iqd6z" class="pc-desc">
-                  Start getting serious
-                  <br />
-                </div>
-                <div id="ipxvv" class="pc-feature odd-feat">
-                  + Hand-picked beer selections each month
-                  <br />
-                </div>
-                <div id="i4w5qp" class="pc-feature odd-feat">
-                  + 25% discount on all of our selection
-                  <br />
-                </div>
-                <div id="iaucg" class="pc-feature">
-                  + 12 litres of the best beer per month
-                  <br />
-                </div>
-                <div id="ia14qo" class="pc-feature">
-                  + Stories of how the beer was made
-                  <br />
-                </div>
-                <div id="iknjf" class="pc-amount odd-feat">
-                  $ 49,90/mo
-                </div>
-              </div>
+            <div id="i0gcu" class="flex-desc">
+              Tasty and different, all at one touch of your fingertips away
+              <br />
             </div>
-            <div class="price-card-cont">
-              <div id="io46x" class="price-card pc-enterprise">
-                <div id="i66um" class="pc-title">
-                  General Beer
-                  <br />
-                </div>
-                <div id="ie10l" class="pc-desc">
-                  Be THE Beer guy
-                  <br />
-                </div>
-                <div id="ir21u" class="pc-feature odd-feat">
-                  + Hand-picked beer selections each month
-                  <br />
-                </div>
-                <div id="ix8xfj" class="pc-feature odd-feat">
-                  + 25% discount on all of our selection
-                  <br />
-                </div>
-                <div id="iv0x8" class="pc-feature">
-                  + 18 litres of the best beer per month
-                </div>
-                <div id="i4ipu" class="pc-feature odd-feat">
-                  + Some of the best snacks
-                  <br />
-                </div>
-                <div id="ia6qu" class="pc-feature">
-                  + Stories of how the beer was made
-                  <br />
-                </div>
-                <div id="i1eyi" class="pc-amount odd-feat">
-                  $ 69,90/mo
-                </div>
-              </div>
+            <div class="cards">
+              {this.state.items.map((item) => (
+                <Card
+                  imgurl={item.imgurl}
+                  name={item.name}
+                  description={item.description}
+                />
+              ))}
             </div>
           </div>
-        </div>
-      </section>
-      <section id="iljuq" class="bdg-sect">
-        <div class="container-width">
-          <h1 id="iswqp" class="bdg-title">
-            What people say
-            <br />
-          </h1>
-          <div id="i4k6l" class="badges">
-            <div id="i64zq" class="badge">
-              <div id="igpfhs" class="badge-header"></div>
-              <img src="https://thispersondoesnotexist.com/image" class="badge-avatar" />
-              <div class="badge-body">
-                <div class="badge-name">Adam Smith</div>
-                <div class="badge-role">CEO</div>
-                <div id="i3p5su" class="badge-desc">
-                  I f%$#@g love beer and beerok loves beer too. What more do you
-                  need?
-                  <br />
+        </section>
+        <section id="iwj3k" class="blk-sect">
+          <div class="container-width">
+            <div id="ivwuf" class="blk-title">
+              beerok Subscriptions
+              <br />
+            </div>
+            <div id="i5byh" class="blk-desc">
+              Subscribe to our club and get beer every month
+              <br />
+            </div>
+            <div class="price-cards">
+              <div class="price-card-cont">
+                <div class="price-card">
+                  <div id="i0fki" class="pc-title">
+                    Private Beer
+                    <br />
+                  </div>
+                  <div id="irr1l" class="pc-desc">
+                    If you want to get a taste
+                    <br />
+                  </div>
+                  <div id="iub2s" class="pc-feature odd-feat">
+                    + Hand-picked beer selections each month
+                    <br />
+                  </div>
+                  <div id="i0wmu" class="pc-feature">
+                    + 6 litres of the best beer per month
+                  </div>
+                  <div id="ii3u5h" class="pc-feature">
+                    + Stories of how the beer was made
+                    <br />
+                  </div>
+                  <div id="i38a8" class="pc-amount odd-feat">
+                    $ 29,90/mo
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="badge">
-              <div class="badge-header"></div>
-              <img src="https://thispersondoesnotexist.com/image?3" class="badge-avatar" />
-              <div class="badge-body">
-                <div class="badge-name">John Black</div>
-                <div class="badge-role">Software Engineer</div>
-                <div id="i7vvek" class="badge-desc">
-                  I&#039;ve always been a fan of beer, drinking a little here
-                  and there, you know? It wasn&#039;t serious until I met
-                  beerok, and wow, these guys changed my world!
-                  <br />
+              <div class="price-card-cont">
+                <div id="iv8vn" class="price-card pc-regular">
+                  <div id="i57yh" class="pc-title">
+                    Lieutenant Beer
+                    <br />
+                  </div>
+                  <div id="iqd6z" class="pc-desc">
+                    Start getting serious
+                    <br />
+                  </div>
+                  <div id="ipxvv" class="pc-feature odd-feat">
+                    + Hand-picked beer selections each month
+                    <br />
+                  </div>
+                  <div id="i4w5qp" class="pc-feature odd-feat">
+                    + 25% discount on all of our selection
+                    <br />
+                  </div>
+                  <div id="iaucg" class="pc-feature">
+                    + 12 litres of the best beer per month
+                    <br />
+                  </div>
+                  <div id="ia14qo" class="pc-feature">
+                    + Stories of how the beer was made
+                    <br />
+                  </div>
+                  <div id="iknjf" class="pc-amount odd-feat">
+                    $ 49,90/mo
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="badge">
-              <div class="badge-header"></div>
-              <img src="https://thispersondoesnotexist.com/image?2" class="badge-avatar" />
-              <div class="badge-body">
-                <div class="badge-name">Jessica White</div>
-                <div class="badge-role">Web Designer</div>
-                <div id="i3xypu" class="badge-desc">
-                  Don&#039;t listen to them if they tell you beer is boring! It
-                  has never been more fun than with hand-picked monthly beerok
-                  selections!
-                  <br />
+              <div class="price-card-cont">
+                <div id="io46x" class="price-card pc-enterprise">
+                  <div id="i66um" class="pc-title">
+                    General Beer
+                    <br />
+                  </div>
+                  <div id="ie10l" class="pc-desc">
+                    Be THE Beer guy
+                    <br />
+                  </div>
+                  <div id="ir21u" class="pc-feature odd-feat">
+                    + Hand-picked beer selections each month
+                    <br />
+                  </div>
+                  <div id="ix8xfj" class="pc-feature odd-feat">
+                    + 25% discount on all of our selection
+                    <br />
+                  </div>
+                  <div id="iv0x8" class="pc-feature">
+                    + 18 litres of the best beer per month
+                  </div>
+                  <div id="i4ipu" class="pc-feature odd-feat">
+                    + Some of the best snacks
+                    <br />
+                  </div>
+                  <div id="ia6qu" class="pc-feature">
+                    + Stories of how the beer was made
+                    <br />
+                  </div>
+                  <div id="i1eyi" class="pc-amount odd-feat">
+                    $ 69,90/mo
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-    </div>
-  );
-};
+        </section>
+        <section id="iljuq" class="bdg-sect">
+          <div class="container-width">
+            <h1 id="iswqp" class="bdg-title">
+              What people say
+              <br />
+            </h1>
+            <div id="i4k6l" class="badges">
+              <div id="i64zq" class="badge">
+                <div id="igpfhs" class="badge-header"></div>
+                <img
+                  src="https://thispersondoesnotexist.com/image"
+                  class="badge-avatar"
+                />
+                <div class="badge-body">
+                  <div class="badge-name">Adam Smith</div>
+                  <div class="badge-role">CEO</div>
+                  <div id="i3p5su" class="badge-desc">
+                    I f%$#@g love beer and beerok loves beer too. What more do
+                    you need?
+                    <br />
+                  </div>
+                </div>
+              </div>
+              <div class="badge">
+                <div class="badge-header"></div>
+                <img
+                  src="https://thispersondoesnotexist.com/image?3"
+                  class="badge-avatar"
+                />
+                <div class="badge-body">
+                  <div class="badge-name">John Black</div>
+                  <div class="badge-role">Software Engineer</div>
+                  <div id="i7vvek" class="badge-desc">
+                    I&#039;ve always been a fan of beer, drinking a little here
+                    and there, you know? It wasn&#039;t serious until I met
+                    beerok, and wow, these guys changed my world!
+                    <br />
+                  </div>
+                </div>
+              </div>
+              <div class="badge">
+                <div class="badge-header"></div>
+                <img
+                  src="https://thispersondoesnotexist.com/image?2"
+                  class="badge-avatar"
+                />
+                <div class="badge-body">
+                  <div class="badge-name">Jessica White</div>
+                  <div class="badge-role">Web Designer</div>
+                  <div id="i3xypu" class="badge-desc">
+                    Don&#039;t listen to them if they tell you beer is boring!
+                    It has never been more fun than with hand-picked monthly
+                    beerok selections!
+                    <br />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
 
 export default Content;
